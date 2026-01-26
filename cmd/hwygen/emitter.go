e9595c28b065fe27e4687d24e032d398a68650d3
AJ Roetker <ajroetker@antfly.io>
2026-01-26 09:42:14 -0800

    refactor: use blacklist approach for preserved imports
    Instead of whitelisting specific stdlib packages (fmt, unsafe, math/bits,
    encoding/binary), use a blacklist of hwy-related packages that get
    transformed by hwygen.
    
    This allows users to use any stdlib or third-party package in their
    base functions without needing to add them to a whitelist.
    
    Blacklisted (transformed) imports:
    - github.com/ajroetker/go-highway/hwy
    - github.com/ajroetker/go-highway/hwy/asm
    - github.com/ajroetker/go-highway/hwy/contrib/algo
    - github.com/ajroetker/go-highway/hwy/contrib/math

--- cmd/hwygen/emitter.go
+++ cmd/hwygen/emitter.go
@@ -720,16 +720,21 @@ func EmitTarget(funcs []*ast.FuncDecl, target Target, pkgName, baseName, outPath
 	// Walk the AST to find which packages are actually referenced
 	usedPkgs := collectUsedPackages(funcs)
 
-	// These are stdlib packages that don't get transformed (unlike hwy -> asm)
-	preservedImports := map[string]bool{
-		"fmt":             true,
-		"unsafe":          true,
-		"math/bits":       true,
-		"encoding/binary": true,
+	// Use blacklist approach: preserve all imports EXCEPT hwy-related ones that get transformed.
+	// This allows users to use any stdlib or third-party package without needing to whitelist it.
+	transformedImports := map[string]bool{
+		"github.com/ajroetker/go-highway/hwy":              true,
+		"github.com/ajroetker/go-highway/hwy/asm":          true,
+		"github.com/ajroetker/go-highway/hwy/contrib/algo": true,
+		"github.com/ajroetker/go-highway/hwy/contrib/math": true,
 	}
-	// First, add imports that were present in the source and are still used
+
+	// Preserve imports that are used and not transformed by hwygen
 	for localName, importPath := range sourceImports {
-		if preservedImports[importPath] && usedPkgs[localName] {
+		if transformedImports[importPath] {
+			continue // Skip hwy-related imports that get transformed
+		}
+		if usedPkgs[localName] {
 			if localName == importPath || localName == "" || localName == filepath.Base(importPath) {
 				imports = append(imports, fmt.Sprintf(`"%s"`, importPath))
 			} else {
